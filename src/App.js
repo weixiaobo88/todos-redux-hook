bfcefe1d0807e48dc8450cc5eb2479a98159a0f2
devxu <devxu@thoughtworks.com>
2020-10-28 18:32:26 +0800

    [jeany] refactor: add different way of implement router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 import TodoListContainer from "./containers/TodoListContainer";
 import DoneListContainer from "./containers/DoneListContainer";
-import {Route, Link, HashRouter} from "react-router-dom";
+import NotFoundPage from "./components/NotFoundPage"
+import {Route, Link, HashRouter, BrowserRouter, Switch} from "react-router-dom";
 
 function App() {
   return (
@@ -10,14 +11,29 @@ function App() {
           <header className="App-header">
               <p>Hello React!</p>
           </header>
-          <HashRouter>
+          {/*<HashRouter>*/}
+          {/*    <ul>*/}
+          {/*        <li><Link to="/">go to list page</Link></li>*/}
+          {/*        <li><Link to="/done">go to done page</Link></li>*/}
+          {/*    </ul>*/}
+          {/*    <Route exact path="/" component={TodoListContainer}/>*/}
+          {/*    <Route path="/done" component={DoneListContainer}/>*/}
+          {/*</HashRouter>*/}
+          <BrowserRouter>
               <ul>
-                  <li><Link to="/">go to list page</Link></li>
-                  <li><Link to="/done">go to done page</Link></li>
+                  <li>
+                      <Link to="/"> go to list page </Link>
+                  </li>
+                  <li>
+                      <Link to="/done"> go to done page </Link>
+                  </li>
               </ul>
-              <Route exact path="/" component={TodoListContainer}/>
-              <Route path="/done" component={DoneListContainer}/>
-          </HashRouter>
+              <Switch>
+                  <Route exact path="/" component={TodoListContainer}></Route>
+                  <Route exact path="/done" component={DoneListContainer}></Route>
+                  <Route path="*" component={NotFoundPage}></Route>
+              </Switch>
+          </BrowserRouter>
       </React.Fragment>
   );
 }
